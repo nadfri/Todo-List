d01f1ec36bcbe350f0e1157c0e78ee0b1a034d62
Nader Frigui <nadfri@gmail.com>
2020-02-21 01:34:07 +0100

    refactoring OK

--- script.js
+++ script.js
@@ -1,69 +1,13 @@
-/********************Functions to use on elements created by JS*************************/
-const divControlEffects = (text, color) =>{
-  divControl.style.display         = "none";
-  divControl.style.display         = "";
-  divControl.innerHTML             = text;
-  divControl.style.backgroundColor = color;
-  divControl.className             = "effects";
-
-  setTimeout(() => {
-    divControl.innerHTML             = "";
-    divControl.style.backgroundColor = "";
-    divControl.className             = "";
-  }, 3000); 
-};
-
-const task_delete  = "<i>Tâche supprimée avec succès!</i>";
-const task_history = "<i>Félicitations! tâche deplacée dans l'historique...</i>";
-/********************************************************************/
-const setUrgent = (element) => {
-  element.style.opacity = (element.style.opacity != "1") ? "1" : "0.3";
-};
-
-/********************************************************************/
-const deleteTask =(element,text,color)=>{
-  element.closest("p").className = "effectsOut";
-  setTimeout(() => {element.closest("p").remove();},400);
-  divControlEffects(text, color);
-
-};
-
-/********************************************************************/
-const saveTask = (element) => {
-
-  const del    = `<span class ='options' onclick="deleteTask(this,task_delete,'pink')"
-                   title ='Cliquez ici pour supprimer une tâche'>❌</span>`;
-
-  const dateNow = () => { //return date in the good format
-    const date = new Date();
-    let day    = (date.getDate()        < 10) ? "0" + date.getDate() : date.getDate();
-    let month  = ((date.getMonth() + 1) < 10) ? "0" + (date.getMonth() + 1) : (date.getMonth() + 1);
-    let hour   = (date.getHours()       < 10) ? "0" + date.getHours() : date.getHours();
-    let minute = (date.getMinutes()     < 10) ? "0" + date.getMinutes() : date.getMinutes();
-    return `${day}/${month}/${date.getFullYear()} à ${hour}:${minute}`;
-  };
-
-  const regex = /<z>(.*)<\/z>/; //use false balise <z> to catch the text of Task
-  let textTask = element.closest("p").innerHTML.match(regex)[0];
-  
-  fieldsetHist.appendChild(document.createElement("p")).innerHTML = `${textTask}
-                              <span class='tab2'>
-                                <i>Fait le: ${dateNow()}</i> ${del}
-                              </span>`;
-
-  element.innerHTML ="";
-
-  deleteTask(element,task_history,"#BBF7CA");
-};
-
 /***********************Start of window.onload*****************************************/
 window.onload = function () {
 
-  const check        = `<span class = 'options' onclick="saveTask(this)"  
+  const check        = `<span class = 'check options' 
                         title = 'Cliquez ici si cette tâche est terminée'>✔️</span>`;
-  const urgent       = `<span class = 'options' onclick="setUrgent(this)" 
-                        title = 'Cliquer ici si la tâche est urgente'>⭐</span>`
-  const del          = `<span class = 'options' onclick="deleteTask(this,task_delete,'pink')"
+
+  const urgent       = `<span class = 'urgent options'
+                        title = 'Cliquer ici si la tâche est urgente'>⭐</span>`;
+
+  const del          = `<span class = 'del options'
                         title = 'Cliquez ici pour supprimer une tâche'>❌</span>`;
 
   const task_empty   = "<i>Tâche vide, rien n'a été ajouté à votre liste...</i>";
@@ -71,27 +15,29 @@ window.onload = function () {
   const task_delete  = "<i>Tâche supprimée avec succès!</i>";
   const task_history = "<i>Félicitations! tâche deplacée dans l'historique...</i>";
 
-/*******************************************/
-  input.onkeyup = () => { if (event.keyCode == 13) add(); };
-
-  const add = () => {
-    if (input.value == "") divControlEffects(task_empty, "pink");
-      
-    else
+/************************Add New Task*******************/
+  input.onkeyup = () => 
+  { 
+    if (event.keyCode == 13) //Confirm with ENTRY key (13)
     {
-      divControlEffects(task_success, "#BBF7CA");
+      if (input.value == "") divControlEffects(task_empty, "pink");
+      
+      else
+      {
+        divControlEffects(task_success, "#BBF7CA");
 
-      fieldsetTask.appendChild(document.createElement("p")).innerHTML= `${check} <z>${input.value}</z> 
+        fieldsetTask.appendChild(document.createElement("p")).innerHTML = `${check} <z>${input.value}</z> 
                                     <span class='tab'>
                                       <i>Ajouté le: ${dateNow()}</i>${urgent}${del}
                                     </span>`;
-      input.value = "";
-      input.focus();
+        input.value = "";
+        input.focus();
+      }
     }
 
   };
 
- /********************************************/
+ /**********************Add a date of Task**********************/
   const dateNow = () => { //return date in the good format
     const date = new Date();
     let day    = (date.getDate()        < 10) ? "0" + date.getDate() : date.getDate();
@@ -101,13 +47,71 @@ window.onload = function () {
     return `${day}/${month}/${date.getFullYear()} à ${hour}:${minute}`;
   };
 
-  /*******************************************/
-  btHistoric.onclick = () =>{
+  /****************************Div Effects**********************************************/
+  const divControlEffects = (text, color) =>{
+    divControl.style.display         = "none";
+    divControl.style.display         = "";
+    divControl.innerHTML             = text;
+    divControl.style.backgroundColor = color;
+    divControl.className             = "effects";
+
+    setTimeout(() => {
+      divControl.innerHTML             = "";
+      divControl.style.backgroundColor = "";
+      divControl.className             = "";
+    }, 3000);    
+  };
+
+  /*****************************Delete Task*********************************************/
+
+  function deleteTask(element,text,color)
+  {
+    element.target.closest("p").className = "effectsOut";
+    setTimeout(() => {element.target.closest("p").remove();},500);
+    divControlEffects(text, color);
+  }
+
+    /***************************Delete Task on Historic**********************************/
+    fieldsetHist.onclick = (element) =>
+    {
+        if (element.target.className == "del options")
+          deleteTask(element, task_delete, "pink");
+    };
+
+  /******************************Running of options for the Task*************************/
+    fieldsetTask.onclick = (element) =>
+    {
+        if (element.target.className == "del options")
+        {
+          deleteTask(element, task_delete, "pink");
+        }
+
+        else if (element.target.className == "urgent options")
+          element.target.style.opacity = (element.target.style.opacity != "1") ? "1" : "0.3";
+          
+
+        else if (element.target.className == "check options")
+        {
+          const regex = /<z>(.*)<\/z>/; //use false balise <z> to catch the text of Task
+          let textTask = element.target.closest("p").innerHTML.match(regex)[0];
+          fieldsetHist.appendChild(document.createElement("p")).innerHTML = `${textTask}
+                              <span class='tab2'>
+                                <i>Fait le: ${dateNow()}</i> ${del}
+                              </span>`;
+
+          element.target.innerHTML =""; //del check button faster to avoid bug in historic
+          deleteTask(element,task_history,"#BBF7CA");
+        }
+
+    };
+
+   /********************Display of Historic***********************/
+   btHistoric.onclick = () =>{
     
     if(btHistoric.textContent    == "Historique")
     {
       fieldsetTask.style.display = "none";
-      fieldsetHist.style.display = "inherit";
+      fieldsetHist.style.display = "block";
       btHistoric.textContent     = "Tâches";
       input.disabled             = true;
       input.placeholder          = "Cliquez sur Tâches pour revenir à la liste en cours...";
@@ -125,21 +129,15 @@ window.onload = function () {
       
     }
   };
+  
 
-  /*******************************************/
-  const divControlEffects = (text, color) =>{
-    divControl.style.display         = "none";
-    divControl.style.display         = "";
-    divControl.innerHTML             = text;
-    divControl.style.backgroundColor = color;
-    divControl.className             = "effects";
 
-    setTimeout(() => {
-      divControl.innerHTML             = "";
-      divControl.style.backgroundColor = "";
-      divControl.className             = "";
-    }, 3000);    
-  };
+
+
+
+
+
+
 
 
 
